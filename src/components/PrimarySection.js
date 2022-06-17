8fb640e6372f8965a72bb333cdcaf5df643ccb7f
Ali Sbane <ali.sbane.dev@gmail.com>
2022-06-18 00:47:36 +0100

    Making a primary section styled compoenent to use on every single page as all have the same backgroud

---
+++ src/components/PrimarySection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styled from "styled-components";
+
+// images
+// images
+import TopBlob from "../assets/images/top-blob.svg";
+import BottomBlob from "../assets/images/bottom-blob.svg";
+
+const PrimarySection = styled.section`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: url("${TopBlob}") no-repeat top right,
+    url("${BottomBlob}") no-repeat bottom left;
+  background-color: #f5f7fb;
+`;
+
+export default PrimarySection;
--- src/pages/GetStarted.js
+++ src/pages/GetStarted.js
@@ -1,22 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-// images
-import TopBlob from "../assets/images/top-blob.svg";
-import BottomBlob from "../assets/images/bottom-blob.svg";
-
 // styled components
 import PrimaryButton from "../components/PrimaryButton";
+import PrimarySection from "../components/PrimarySection";
 
-const Section = styled.section`
-  min-height: 100vh;
+const Section = styled(PrimarySection)`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: url("${TopBlob}") no-repeat top right,
-    url("${BottomBlob}") no-repeat bottom left;
-  background-color: #f5f7fb;
 `;
 
 const Title = styled.h1`
